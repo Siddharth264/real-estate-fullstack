e1d425832a8e3b8655a1c51bd084f0f3e9760e09
Siddharth Singh <sidsingh264@gmail.com>
2023-11-02 00:42:02 +0530

    Header Created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,9 +4,11 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
+import Header from './components/Header';
 function App() {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
