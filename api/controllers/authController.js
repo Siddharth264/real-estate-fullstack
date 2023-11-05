31635a71a830aba26db5441af51064a66bc43d4a
Siddharth Singh <sidsingh264@gmail.com>
2023-11-06 03:23:06 +0530

    redux and redux-persist added

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require('../models/userModel');
 const bcryptjs = require('bcryptjs');
 const errorHandler = require('../utils/errors')
+const jwt = require('jsonwebtoken')
 const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -15,4 +16,21 @@ const signup = async (req, res, next) => {
     
 }
 
-module.exports = {signup}
+const signin = async (req, res, next) => {
+    const {email, password} = req.body;
+    try{
+        const validUser = await User.findOne({email})
+        if(!validUser) return next(errorHandler(404, 'User not found')); 
+
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+
+        if(!validPassword) return next(errorHandler(401, 'Incorrect password'));
+
+        const token = jwt.sign({id: validUser._id },process.env.JWT_SECRET)
+        res.cookie('access_token', token, {httpOnly: true}).status(200).json(validUser)
+    }catch(error){
+        next(error);
+    }
+}
+
+module.exports = {signup, signin}
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,6 +1,82 @@
-
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from 'react-redux';
+import {signInStart, signInSuccess, signInFailure} from '../redux/user/userSlice'
 export default function SignIn() {
+  const [formData, setFormData] = useState({});
+  const { loading, error} = useSelector((state)=> state.user)
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      //setLoading(true);
+      dispatch(signInStart())
+      const res = await fetch("/api/v1/auth/signin", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await res.json();
+      if (data.success === false) {
+        //setError(data.message);
+        //setLoading(false);
+        dispatch(signInFailure(data.message));
+        return;
+      }
+      // setLoading(false);
+      // setError(null);
+      dispatch(signInSuccess(data));
+      navigate("/");
+    } catch (err) {
+      // setLoading(false);
+      // setError(err.message);
+      dispatch(signInFailure(err.message));
+    }
+  };
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
   return (
-    <div><h1>SignIn</h1></div>
-  )
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-bold my-7">Sign In</h1>
+
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        
+        <input
+          onChange={handleChange}
+          type="email"
+          placeholder="email"
+          className="border p-3 rounded-xs"
+          id="email"
+        />
+        <input
+          onChange={handleChange}
+          type="password"
+          placeholder="password"
+          className="border p-3 rounded-xs"
+          id="password"
+        />
+
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:bg-slate-800 disabled:bg-slate-500"
+        >
+          {loading ? "Loading..." : "Sign In"}
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p> Dont have an account? </p>
+        <Link to="/sign-up">
+          <span className="text-blue-700">Sign-up</span>
+        </Link>
+      </div>
+      {error && <p className="text-red-400 mt-5">{error}</p>}
+    </div>
+  );
 }
